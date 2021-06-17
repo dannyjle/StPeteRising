860bfd4f6b5c760e2e32e5d80d843b31552c7550
Danny Le <le.dannyj@gmail.com>
2021-06-17 11:58:52 -0400

    edited model

--- Models/Project.cs
+++ Models/Project.cs
@@ -7,8 +7,8 @@ namespace StPeteRising.Models
         public string Status { get; set; }
         public string Address { get; set; }
         public string Class { get; set; }
-        public string Floor { get; set; }
-        public string Units { get; set; }
+        public int Floor { get; set; }
+        public int Units { get; set; }
         public string Completion { get; set; }
         public string Website { get; set; }
     }
