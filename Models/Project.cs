68f3967d3e9a3e6ba5d8d7af9fb6f8b1f126aa0d
Danny Le <le.dannyj@gmail.com>
2021-06-17 11:53:22 -0400

    created model

---
+++ Models/Project.cs
@@ -0,0 +1,15 @@
+namespace StPeteRising.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Address { get; set; }
+        public string Class { get; set; }
+        public string Floor { get; set; }
+        public string Units { get; set; }
+        public string Completion { get; set; }
+        public string Website { get; set; }
+    }
+}
