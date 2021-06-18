7a5c933aed6ca30d8799eba386a99f1856c04f9a
Danny Le <le.dannyj@gmail.com>
2021-06-18 11:15:58 -0400

    Added Example Data

---
+++ Models/ExampleData.sql
@@ -0,0 +1,10 @@
+TRUNCATE TABLE "Projects";
+
+INSERT INTO "Projects" ("Name", "Status", "Address", "Class", "Floor", "Units", "Completion", "Website") VALUES ('Avanti', 'Complete', '201 4th St S', 'Apartments', '9', '366', '2018', 'https://avantistpete.com/');
+INSERT INTO "Projects" ("Name", "Status", "Address", "Class", "Floor", "Units", "Completion", "Website") VALUES ('ONE St. Petersburg', 'Complete', '100 1st Ave N', 'Condos', '41', '253', '2018', 'http://onestpetersburg.com/');
+INSERT INTO "Projects" ("Name", "Status", "Address", "Class", "Floor", "Units", "Completion", "Website") VALUES ('Tru by Hilton', 'Complete', '1650 Central Ave', 'Hotel', '7', '132', '2020', 'https://www.hilton.com/en/hotels/piedtru-tru-st-petersburg-downtown-central-ave/');
+INSERT INTO "Projects" ("Name", "Status", "Address", "Class", "Floor", "Units", "Completion", "Website") VALUES ('Saint James Townhomes', 'Complete', '758 3rd Ave S', 'Townhomes', '3', '8', '2020', 'https://saintjamessaintpete.com/');
+INSERT INTO "Projects" ("Name", "Status", "Address", "Class", "Floor", "Units", "Completion", "Website") VALUES ('2800 Central Ave', 'Proposed', '2800 Central Ave', 'Condos', '5', '16', '2022', 'https://www.2800central.com/');
+INSERT INTO "Projects" ("Name", "Status", "Address", "Class", "Floor", "Units", "Completion", "Website") VALUES ('Bezu', 'Cancelled', '100 4th Ave N', 'Residential', '24', '28', '2019', 'N/A');
+INSERT INTO "Projects" ("Name", "Status", "Address", "Class", "Floor", "Units", "Completion", "Website") VALUES ('Ascent St Petersburg', 'Under Construction', '225 1st Ave N', 'Apartments/Hotel', '36', '526', '2023', 'N/A');
+INSERT INTO "Projects" ("Name", "Status", "Address", "Class", "Floor", "Units", "Completion", "Website") VALUES ('Sapphire Condominiums', 'Proposed', '602 3rd Ave S', 'Condos', '6', '11', 'TBD', 'N/A');
