92e5c4778839337595efdde9f658f01653f048cf
Danny Le <le.dannyj@gmail.com>
2021-06-23 11:55:01 -0400

    Exported Layout to be used multuple times

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import './custom.scss'
-import { Home } from './pages'
-import { Project } from './pages'
-import { AddNew } from './pages'
-import { Admin } from './pages'
+import { Home, Project, AddNew, Admin, Layout } from './pages'
 
 export function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/project/:id">
-        <Project />
-      </Route>
-      <Route exact path="/admin">
-        <Admin />
-      </Route>
-      <Route exact path="/add">
-        <AddNew />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/project/:id">
+          <Project />
+        </Route>
+        <Route exact path="/admin">
+          <Admin />
+        </Route>
+        <Route exact path="/add">
+          <AddNew />
+        </Route>
+        <Route path="*">Not Found</Route>
+      </Switch>
+    </Layout>
   )
 }
