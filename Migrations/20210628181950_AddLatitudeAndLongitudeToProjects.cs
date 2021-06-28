bf5763d87a4d7d73a8d43f763b758b59d1cc3bc0
Danny Le <le.dannyj@gmail.com>
2021-06-28 14:21:49 -0400

    Lad and Longitude migration

---
+++ Migrations/20210628181950_AddLatitudeAndLongitudeToProjects.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StPeteRising.Migrations
+{
+    public partial class AddLatitudeAndLongitudeToProjects : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Projects",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Projects",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Projects");
+        }
+    }
+}
--- Models/Project.cs
+++ Models/Project.cs
@@ -24,5 +24,9 @@ namespace StPeteRising.Models
         public string Website { get; set; }
 
         public int UserId { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
     }
 }
