de024388040adc7075e659048978d37e5fd63500
Danny Le <le.dannyj@gmail.com>
2021-06-29 17:35:36 -0400

    No more console errors

--- ClientApp/src/pages/Project.jsx
+++ ClientApp/src/pages/Project.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Link, useHistory, useParams } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
 import ReactMapGL, { Marker, NavigationControl, Popup } from 'react-map-gl'
 import { authHeader, getUserId, isLoggedIn } from '../auth'
 
